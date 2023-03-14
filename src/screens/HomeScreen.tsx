12b5545f6f4540aa9e6bdf41bdf59ab6ee2aacc8
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-03-14 00:49:59 -0600

    Mostrando El poster de la pelicula

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import { ActivityIndicator, Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { MoviePoster } from '../components/MoviePoster';
 import { useMovies } from '../hooks/useMovies';
 
 
@@ -8,6 +10,7 @@ export const HomeScreen = () => {
   const { peliculasEnCine, isLoading } = useMovies();
   //console.log(peliculasEnCine[0]?.title)
 
+  const { top }= useSafeAreaInsets(); 
   if( isLoading){
     return(
       <View style={{flex: 1, justifyContent:'center', alignContent:'center'}}>
@@ -16,8 +19,11 @@ export const HomeScreen = () => {
     )
   }
   return (
-    <View>
-        <Text>HomeScreen</Text>
+    <View style={{marginTop:top+20}}>
+        <MoviePoster
+          movie={peliculasEnCine[0]}
+        />
+        
     </View>
     
   )
