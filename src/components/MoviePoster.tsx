d7d5728d82aeb45e9f803ed6b54d04aa3d2dc158
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-03-14 19:51:20 -0600

    FlatList

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -4,17 +4,20 @@ import { Movie } from '../interfaces/movieInterface'
 
 interface Props {
     movie: Movie;
+    height?: number;
+    width?: number;
 }
 
-export const MoviePoster = ({ movie }: Props) => {
+export const MoviePoster = ({ movie, height=420, width=300 }: Props) => {
 
     const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
-    console.log(movie.poster_path);
+    //console.log(movie.poster_path);
   return (
     <View style={{
-        width:300,
-        height:420,
+        width,
+        height,
+        marginHorizontal: 8
 
     }}>
         {/* <Text>{ movie.title}</Text> */}
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react'
-import { ActivityIndicator, Dimensions, Text, View } from 'react-native'
+import { ActivityIndicator, Dimensions, ScrollView, Text, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { MoviePoster } from '../components/MoviePoster';
 import { useMovies } from '../hooks/useMovies';
 import Carousel from 'react-native-snap-carousel'; 
+import { FlatList } from 'react-native';
 
 const {width: windowWidth }= Dimensions.get('window'); 
 //const windowWidth = Dimensions.get('window').width; 
@@ -22,23 +23,44 @@ export const HomeScreen = () => {
     )
   }
   return (
-    <View style={{marginTop:top+20}}>
+    <ScrollView>
+      <View style={{marginTop:top+20}}>
         {/* <MoviePoster
           movie={peliculasEnCine[0]}
         /> */}
         <View style={{
           height:440
         }}>
+          {/* muestra el carrusel de poster de peliculas */}
           <Carousel 
-            data={peliculasEnCine }
+            data={peliculasEnCine } //la informacion viene en peliculasEnCine
             renderItem={ ({item}: any )=> <MoviePoster movie={item }/> }
             sliderWidth={windowWidth }
             itemWidth={300}
             
           />
-
+  
         </View>
-    </View>
+
+        {/* Peliculas Populares */}
+        <View
+          style={{backgroundColor:'red', height:260}}
+        >
+          <Text style={{fontSize:30, fontWeight:'bold'}}>Peliculas Populares</Text>
+          <FlatList
+            data={peliculasEnCine}
+            renderItem={({item}: any )=> (
+              <MoviePoster movie={item } width={140} height={200}/>
+            )}
+            keyExtractor={(item) => item.id.toString()}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+          />
+
+        </View>   
+      </View>
+    </ScrollView>
+    
     
   )
 }
